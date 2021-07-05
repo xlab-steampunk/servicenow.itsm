e2182816366617a6836f4de6ab778e4148c194e9
matejpevec <matej2000@gmail.com>
2021-07-05 14:40:00 +0200

    Add new validation.py functions For use with properties that require other properties to be/not be in a certain state.

--- plugins/module_utils/validation.py
+++ plugins/module_utils/validation.py
@@ -38,3 +38,61 @@ def missing_from_params_and_remote(params, module_params, record=None):
             continue
         missing.append(p)
     return missing
+
+
+def check_value_compatibility(compatible_states, property_name, params, record=None):
+    """
+    Given a list of compatible states, a property name, params dict and a ServiceNow record, returns the check result
+    and value of the checked property. Check result is True if property values are in the compatible state list and
+    False otherwise. The function assumes that new parameters override the record. If the checked property doesn't
+    have a value, this function returns False and None as the checked property value.
+    """
+    property_value = params.get(property_name)
+    # If the property exists and is in a compatible state
+    if property_value in compatible_states:
+        return True, property_value
+    # If the property doesn't exist, but may be set in the record
+    elif property_value is None and record is not None:
+        record_value = record.get(property_name)
+        # If the property isn't set anywhere
+        if record_value is None:
+            return False, None
+        property_value = record_value
+        # If a record exists and the desired property isn't in a compatible state
+        if property_value in compatible_states:
+            return True, property_value
+        # If a record exists and is in a compatible state
+        else:
+            return False, property_value
+    # If the property is not in a compatible state
+    else:
+        return False, property_value
+
+
+def check_value_incompatibility(incompatible_states, property_name, params, record=None):
+    """
+    Given a list of incompatible states, a property name, params dict and a ServiceNow record, returns the check
+    result and value of the checked property. Check result is True if property values aren't in the incompatible
+    state list and False otherwise. The function assumes that new parameters override the record. If the checked
+    property doesn't have a value, this function returns False and None as the checked property value.
+    """
+    property_value = params.get(property_name)
+    # If the property exists and is in an incompatible state
+    if property_value in incompatible_states:
+        return False, property_value
+    # If the property doesn't exist, but may be set in the record
+    elif property_value is None and record is not None:
+        record_value = record.get(property_name)
+        # If the property isn't set anywhere
+        if record_value is None:
+            return False, None
+        property_value = record_value
+        # If a record exists and the desired property isn't in a compatible state
+        if property_value in incompatible_states:
+            return False, property_value
+        # If a record exists and is in a compatible state
+        else:
+            return True, property_value
+    # If the property is in an acceptable state
+    else:
+        return True, property_value
